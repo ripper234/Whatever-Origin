fca70141a79a80412f8c9ea42b4ded93c6115121
Ron Gross <ron.gross@gmail.com>
2011-10-27 01:59:12 +0200

    Fixed callback mechanism. It works!

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.apache.commons.lang.StringUtils;
 import play.libs.F;
 import play.libs.WS;
 import play.mvc.*;
@@ -12,6 +13,7 @@ public class Application extends Controller {
         public Result(String contents) {
             this.contents = contents;
         }
+
         public final String contents;
     }
 
@@ -19,15 +21,33 @@ public class Application extends Controller {
         render();
     }
 
-    public static void get(String url) {
-        F.Promise<WS.HttpResponse> remoteCall = WS.url(url).getAsync();
+    public static void get(String url, final String callback) {
+            F.Promise<WS.HttpResponse> remoteCall = WS.url(url).getAsync();
 
         await(remoteCall, new F.Action<WS.HttpResponse>() {
             public void invoke(WS.HttpResponse result) {
-                String json = new GsonBuilder().disableHtmlEscaping().create().toJson(new Result(result.getString()));
-                response.contentType = "application/javascript";
-                renderJSON(json);
+                String replace = result.getString().replace("/", "\\/");
+
+                String responseStr = new GsonBuilder().disableHtmlEscaping().create().toJson(new Result(replace));
+
+                // http://blog.altosresearch.com/supporting-the-jsonp-callback-protocol-with-jquery-and-java/
+                if ( callback != null ) {
+                    response.contentType = "application/x-javascript";
+                    responseStr = callback + "(" + responseStr + ");";
+                } else {
+                    response.contentType = "application/json";
+                }
+
+                renderJSON(responseStr);
             }
         });
+    }
+
+    public static String sanitizeJsonpParam(String s) {
+        if (StringUtils.isEmpty(s)) return null;
+        if (!StringUtils.startsWithIgnoreCase(s, "jsonp")) return null;
+        if (StringUtils.length(s) > 128) return null;
+        if (!s.matches("^jsonp\\d+$")) return null;
+        return s;
     }
 }
