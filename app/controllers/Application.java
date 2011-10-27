73e8d626d79700b49fc4281689d739b1497e0d8b
Ron Gross <ron.gross@gmail.com>
2011-10-27 03:07:10 +0200

    Add a few fields to the json

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.apache.commons.lang.StringUtils;
 import play.libs.F;
@@ -10,30 +9,46 @@ import play.mvc.*;
 public class Application extends Controller {
 
     public static class Result {
-        public Result(String contents) {
+        public Result(String contents, Status status) {
             this.contents = contents;
+            this.status = status;
         }
 
         public final String contents;
+        public final Status status;
+
+        // "status":{"url":"https:\/\/mtgox.com\/api\/0\/data\/ticker.php","content_type":"application\/json","http_code":200
+
+        public static class Status {
+            public final String url;
+            public final String content_type;
+            public final int http_code;
+
+            public Status(String url, String content_type, int http_code) {
+                this.url = url;
+                this.content_type = content_type;
+                this.http_code = http_code;
+            }
+        }
     }
 
     public static void index() {
         render();
     }
 
-    public static void get(String url, final String callback) {
+    public static void get(final String url, final String callback) {
             F.Promise<WS.HttpResponse> remoteCall = WS.url(url).getAsync();
 
         await(remoteCall, new F.Action<WS.HttpResponse>() {
             public void invoke(WS.HttpResponse result) {
                 String replace = result.getString().replace("/", "\\/");
 
-                String responseStr = new GsonBuilder().disableHtmlEscaping().create().toJson(new Result(replace));
+                String responseStr = new GsonBuilder().disableHtmlEscaping().create().toJson(new Result(replace, new Result.Status(url, result.getContentType(), result.getStatus())));
 
                 // http://blog.altosresearch.com/supporting-the-jsonp-callback-protocol-with-jquery-and-java/
                 if ( callback != null ) {
                     response.contentType = "application/x-javascript";
-                    responseStr = callback + "(" + responseStr + ");";
+                    responseStr = callback + "(" + responseStr + ")";
                 } else {
                     response.contentType = "application/json";
                 }
